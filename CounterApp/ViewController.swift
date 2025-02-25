5763f1173d75176629034ed8ccd31d39b56bd24e
ghnn-n <cgh6470@gmail.com>
2025-02-25 15:53:48 +0900

    first commit

---
+++ CounterApp/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  CounterApp
+//
+//  Created by 최규현 on 2/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var plusBtn: UIButton!
+    @IBOutlet weak var minusBtn: UIButton!
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    var num = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func resetButton(_ sender: Any) {
+        num = 0
+        numberLabel.text = String(num)
+        plusBtn.isEnabled = true
+        minusBtn.isEnabled = true
+    }
+    
+    @IBAction func plusButton(_ sender: Any) {
+        num += 1
+        
+        if num >= 10 {
+            plusBtn.isEnabled = false
+            numberLabel.text = String(num)
+        } else {
+            numberLabel.text = String(num)
+        }
+        
+        if !minusBtn.isEnabled {
+            minusBtn.isEnabled = true
+        }
+    }
+    
+    @IBAction func minusButton(_ sender: Any) {
+        num -= 1
+        
+        if num <= -10 {
+            minusBtn.isEnabled = false
+            numberLabel.text = String(num)
+        } else {
+            numberLabel.text = String(num)
+        }
+        
+        if !plusBtn.isEnabled {
+            plusBtn.isEnabled = true
+        }
+    }
+}
+
