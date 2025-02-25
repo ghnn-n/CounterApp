ea49562c6251ee978d97d1797201aeb4f7de0650
ghnn-n <cgh6470@gmail.com>
2025-02-25 16:45:13 +0900

    fix code

--- CounterApp/ViewController.swift
+++ CounterApp/ViewController.swift
@@ -11,8 +11,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var plusBtn: UIButton!
     @IBOutlet weak var minusBtn: UIButton!
     @IBOutlet weak var numberLabel: UILabel!
-    
+        
     var num = 0
+    
+    func clickButton() {
+        numberLabel.text = String(num)
+        
+        minusBtn.isEnabled = num > -10
+        plusBtn.isEnabled = num < 10
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,39 +28,43 @@ class ViewController: UIViewController {
 
     @IBAction func resetButton(_ sender: Any) {
         num = 0
-        numberLabel.text = String(num)
-        plusBtn.isEnabled = true
-        minusBtn.isEnabled = true
+        clickButton()
+        
+//        numberLabel.text = String(num)
+//        plusBtn.isEnabled = true
+//        minusBtn.isEnabled = true
     }
     
     @IBAction func plusButton(_ sender: Any) {
         num += 1
+        clickButton()
         
-        if num >= 10 {
-            plusBtn.isEnabled = false
-            numberLabel.text = String(num)
-        } else {
-            numberLabel.text = String(num)
-        }
-        
-        if !minusBtn.isEnabled {
-            minusBtn.isEnabled = true
-        }
+//        if num >= 10 {
+//            plusBtn.isEnabled = false
+//            numberLabel.text = String(num)
+//        } else {
+//            numberLabel.text = String(num)
+//        }
+//        
+//        if !minusBtn.isEnabled {
+//            minusBtn.isEnabled = true
+//        }
     }
     
     @IBAction func minusButton(_ sender: Any) {
         num -= 1
+        clickButton()
         
-        if num <= -10 {
-            minusBtn.isEnabled = false
-            numberLabel.text = String(num)
-        } else {
-            numberLabel.text = String(num)
-        }
-        
-        if !plusBtn.isEnabled {
-            plusBtn.isEnabled = true
-        }
+//        if num <= -10 {
+//            minusBtn.isEnabled = false
+//            numberLabel.text = String(num)
+//        } else {
+//            numberLabel.text = String(num)
+//        }
+//        
+//        if !plusBtn.isEnabled {
+//            plusBtn.isEnabled = true
+//        }
     }
 }
 
